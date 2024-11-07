c87ed2338d75525ebb630637fbb07aff7b41bd18
ArunSDhamodhar <63781115+ArunSDhamodhar@users.noreply.github.com>
2024-11-07 22:13:49 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,5 @@
 package com.example.demo;
-
+import com.example.otherpackage.Application; // Adjust as necessary
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
